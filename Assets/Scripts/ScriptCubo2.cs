b852b66414677b045b4142114586c4053c3fa866
Hesumo <hesumo@alumnatflorida.es>
2026-05-13 22:39:10 +0200

    [GIAN] Scripts

---
+++ Assets/Scripts/ScriptCubo2.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ScriptCubo : MonoBehaviour
+{
+	public float vidaMaxima = 100f;
+	public float vidaActual;
+	//public int puntosAlRomper = 10;
+
+    // Referencia al sistema de puntos
+    //public SistemaPuntos sistemaPuntos;
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		vidaActual = vidaMaxima;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void RecibirDanio(float cantidad)
+	{
+		vidaActual -= cantidad;
+
+		if (vidaActual <= 0)
+		{
+			vidaActual = 0;
+			// Sumar puntos
+            //sistemaPuntos.SumarPuntos(puntosAlRomper);
+
+			Destroy(gameObject); // elimina el cubo
+		}
+	}
+}
